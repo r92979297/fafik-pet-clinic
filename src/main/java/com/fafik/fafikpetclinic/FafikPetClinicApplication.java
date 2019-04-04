496c4dad51a4f3f351c779dd50b4db9eac5e6641
fafik <r92979297@gmail>
2019-04-04 22:27:26 +0200

    Initial project add

---
+++ src/main/java/com/fafik/fafikpetclinic/FafikPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.fafik.fafikpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FafikPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FafikPetClinicApplication.class, args);
+    }
+
+}
