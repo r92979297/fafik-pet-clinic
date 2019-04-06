d06a749e90d9428f1a2a1751c0e9fe517abcad56
fafik <r92979297@gmail>
2019-04-06 06:27:08 +0200

    added crud interface closes #11

--- pet-clinic-data/src/main/java/com/fafik/fafikpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/fafik/fafikpetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package com.fafik.fafikpetclinic.services;
 
 import com.fafik.fafikpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet,Long>{
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
