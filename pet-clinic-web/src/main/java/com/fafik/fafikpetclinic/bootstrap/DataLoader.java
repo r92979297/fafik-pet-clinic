26f3cca81efad24bebf225e4aca2f0c8cf25f9d9
fafik <r92979297@gmail>
2019-04-15 21:14:44 +0200

    Added petType to bootstrap #32

--- pet-clinic-web/src/main/java/com/fafik/fafikpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/fafik/fafikpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.fafik.fafikpetclinic.bootstrap;
 
 import com.fafik.fafikpetclinic.model.Owner;
+import com.fafik.fafikpetclinic.model.PetType;
 import com.fafik.fafikpetclinic.model.Vet;
 import com.fafik.fafikpetclinic.services.OwnerService;
+import com.fafik.fafikpetclinic.services.PetTypeService;
 import com.fafik.fafikpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,15 +14,26 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService= petTypeService;
     }
 
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
