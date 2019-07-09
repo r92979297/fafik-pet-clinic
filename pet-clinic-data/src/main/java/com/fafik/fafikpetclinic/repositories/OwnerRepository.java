f23bd5e711a0d255fe1edbbebb5a20fb0f6aa28d
r929297 <R92979297@gmail.com>
2019-07-09 22:15:47 +0200

    impl find Owner Closes #54

--- pet-clinic-data/src/main/java/com/fafik/fafikpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/fafik/fafikpetclinic/repositories/OwnerRepository.java
@@ -3,6 +3,9 @@ package com.fafik.fafikpetclinic.repositories;
 import com.fafik.fafikpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner,Long> {
     Owner findByLastName(String lastName);
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/com/fafik/fafikpetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/com/fafik/fafikpetclinic/services/map/OwnerMapService.java
@@ -8,6 +8,7 @@ import com.fafik.fafikpetclinic.services.PetTypeService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -77,5 +78,11 @@ public class OwnerMapService extends AbstractMapService<Owner,Long> implements O
                 .filter(owner -> owner.getLastName().equalsIgnoreCase(lastName))
                 .findFirst()
                 .orElse(null);
+    }
+
+    @Override
+    public List<Owner> findAllByLastNameLike(String lastName) {
+        //todo -impl
+        return null;
     }
 }
