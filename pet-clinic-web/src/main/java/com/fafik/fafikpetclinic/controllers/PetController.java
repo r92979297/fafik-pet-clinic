3c35b4b331d2809d78c368fee23ea4e8eba6e73d
r929297 <R92979297@gmail.com>
2019-07-13 14:46:48 +0200

    adding model attributes for Pet controller #56

---
+++ pet-clinic-web/src/main/java/com/fafik/fafikpetclinic/controllers/PetController.java
@@ -0,0 +1,46 @@
+package com.fafik.fafikpetclinic.controllers;
+
+import com.fafik.fafikpetclinic.model.Owner;
+import com.fafik.fafikpetclinic.model.PetType;
+import com.fafik.fafikpetclinic.services.OwnerService;
+import com.fafik.fafikpetclinic.services.PetService;
+import com.fafik.fafikpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+
+@Controller
+@RequestMapping("/owners/{ownerId}")
+public class PetController {
+
+    private static final String VIEWS_PETS_CREATE_OR_UPDATE_FORM ="pets/createOrUpdatePetForm";
+    private final  OwnerService ownerService;
+    private final PetService petService;
+    private final PetTypeService petTypeService;
+
+    public PetController(OwnerService ownerService, PetService petService, PetTypeService petTypeService) {
+        this.ownerService = ownerService;
+        this.petService = petService;
+        this.petTypeService = petTypeService;
+    }
+
+    @ModelAttribute("types")
+    public Collection<PetType> populatePetTypes(){
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable Long ownerId){
+        return ownerService.findById(ownerId);
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder){
+        dataBinder.setDisallowedFields("id");
+    }
+}
