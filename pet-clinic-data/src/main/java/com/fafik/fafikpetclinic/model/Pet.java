ec247957be40ad6b616f1af474d1e08cbe83a120
r929297 <R92979297@gmail.com>
2019-08-05 08:53:08 +0200

    data validation closes #66

--- pet-clinic-data/src/main/java/com/fafik/fafikpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/fafik/fafikpetclinic/model/Pet.java
@@ -1,6 +1,7 @@
 package com.fafik.fafikpetclinic.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -36,7 +37,8 @@ public class Pet extends BaseEntity {
     @JoinColumn(name = "owner_id")
     private Owner  owner;
 
-    @Column(name = "birth_ate")
+    @Column(name = "birth_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
